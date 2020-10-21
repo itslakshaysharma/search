862eda41c6d652afd2425ac3e9b9cf4e00c4e91d
Lakshya Sharma <lakshya5703@gmail.com>
2020-10-21 19:49:43 +0530

    Update search.py

--- search.py
+++ search.py
@@ -1,1 +1,8 @@
-from sele
+from selenium import webdriver
+from selenium.webdriver.common.keys import keys
+from selenium.webdriver.support.ui import Select
+browser = webdriver.Chrome(r'D:\chromedriver_win32\chromedriver.exe')
+browser.get('https://animesoul.com/cards')
+key = input()
+search = browser.find_element_by_class_name('el_input_inner')
+search.send_keys(key + Keys.RETURN)
