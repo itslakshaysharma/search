c58a58e7dddbe015c28b6d89dafb4be799ac2265
Lakshya Sharma <lakshya5703@gmail.com>
2020-10-21 20:45:11 +0530

    Update search.py

--- search.py
+++ search.py
@@ -4,5 +4,6 @@ from selenium.webdriver.support.ui import Select
 browser = webdriver.Chrome(r'D:\chromedriver_win32\chromedriver.exe')
 browser.get('https://animesoul.com/cards')
 key = input()
-search = browser.find_element_by_class_name('el_input_inner')
+search = browser.find_element_by_class_name('el-input__inner')
 search.send_keys(key + Keys.RETURN)
+tier_div = browser.find_element_by_class_name('el-dropdown').click()
